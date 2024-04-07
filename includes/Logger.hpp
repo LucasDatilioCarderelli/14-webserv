a77b71ca72f7c8727c60675e5b9692e7c51b8e30
LucasDatilioCarderelli <llucascarderelli@gmail.com>
2024-04-07 11:50:13 -0300

    add: Logger.cpp

--- includes/Logger.hpp
+++ includes/Logger.hpp
@@ -1,5 +1,9 @@
+#ifndef LOGGER_HPP
+#define LOGGER_HPP
+
 #include <iostream>
 #include <ctime>
+
 
 class Logger {
 public:
@@ -9,34 +13,13 @@ public:
         ERROR
     };
 
-    void log(const std::string& message, Level level = INFO) {
-        std::string levelStr;
-        std::string colorCode;
-        switch (level) {
-            case INFO:
-                levelStr = "[ INFO ]";
-                colorCode = "\033[1;34m"; // Blue
-                break;
-            case WARNING:
-                levelStr = "[ WARNING ]";
-                colorCode = "\033[1;33m"; // Yellow
-                break;
-            case ERROR:
-                levelStr = "[ ERROR ]";
-                colorCode = "\033[1;31m"; // Red
-                break;
-        }
+    Logger();
+    ~Logger();
 
-        std::string logMessage = getCurrentTime() + " " + levelStr + ": " + message;
-        std::cout << colorCode << logMessage << "\033[0m" << std::endl; // Reset color
-    }
+    void log(const std::string& message, Level level = INFO);
 
 private:
-    std::string getCurrentTime() {
-        time_t now = time(0);
-        struct tm * timeinfo = localtime(&now);
-        char buffer[9];
-        strftime(buffer, 9, "%H:%M:%S", timeinfo);
-        return std::string(buffer);
-    }
+    std::string getCurrentTime();
 };
+
+#endif
---
+++ sources/logger/Logger.cpp
@@ -0,0 +1,34 @@
+#include "Logger.hpp"
+
+Logger::Logger() {}
+Logger::~Logger() {}
+
+void Logger::log(const std::string& message, Level level) {
+    std::string levelStr;
+    std::string colorCode;
+    switch (level) {
+        case INFO:
+            levelStr = "[ INFO ]";
+            colorCode = "\033[1;34m"; // Blue
+            break;
+        case WARNING:
+            levelStr = "[ WARNING ]";
+            colorCode = "\033[1;33m"; // Yellow
+            break;
+        case ERROR:
+            levelStr = "[ ERROR ]";
+            colorCode = "\033[1;31m"; // Red
+            break;
+    }
+
+    std::string logMessage = getCurrentTime() + " " + levelStr + ": " + message;
+    std::cout << colorCode << logMessage << "\033[0m" << std::endl; // Reset color
+}
+
+std::string Logger::getCurrentTime() {
+    time_t now = time(0);
+    struct tm * timeinfo = localtime(&now);
+    char buffer[80];
+    strftime(buffer, 80, "%y/%m/%d %H:%M:%S", timeinfo);
+    return std::string(buffer);
+}
