07acc7f98572360c280249dc66676f463be5f0b7
LucasDatilioCarderelli <llucascarderelli@gmail.com>
2024-04-22 21:51:09 -0300

    update: move includes from server.cpp

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -4,12 +4,15 @@
 #include "webserv.hpp"
 #include "Config.hpp"
 #include "Response.hpp"
+#include "Request.hpp"
+#include "utils.hpp"
 
+#include <poll.h>
 #include <fcntl.h> // for non-blocking sockets
 #include <cerrno> // for errno
-#include <sys/epoll.h> // for multiplexing I/O
+#include <sys/poll.h> // for multiplexing I/O
 
-#define MAXEVENTS 64 // Maximum number of events to be returned by epoll_wait
+#define MAXEVENTS 64 // Maximum number of events to be returned by poll_wait
 
 
 class Server {
